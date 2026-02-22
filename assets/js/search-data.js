95bedcd3d4f6dfad4f21b0b7650c255e8e1099f3
angelsdecon <166234941+angelsdecon@users.noreply.github.com>
2026-02-22 20:32:50 +0000

    Deploying to gh-pages from @ angelsdecon/angelsdecon.github.io@525f428424f91e55f64547406d96a3c6119bee37 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "nav-misc2",
-          title: "misc2",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/misc2/index.html";
-          },
         },{id: "post-tomarse-un-par-de-gin-tonics-puede-aumentar-el-poder-sindical-en-la-negociación-colectiva",
         
           title: '🤝🏼🍹¿Tomarse un par de gin-tonics puede aumentar el poder sindical en la negociación... <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
