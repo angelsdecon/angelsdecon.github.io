c0b77257ecd6dbdf658af9100c9ef2087b057d89
angelsdecon <166234941+angelsdecon@users.noreply.github.com>
2026-03-24 15:41:25 +0000

    Deploying to gh-pages from @ angelsdecon/angelsdecon.github.io@ad27dbda99258463310f608efd91c90585a6e49e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-research",
+  },{id: "nav-cv",
+          title: "cv",
+          description: "what i&#39;ve been doing so far! i&#39;d love to do so many more things!",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "nav-research",
           title: "research",
           description: "",
           section: "Navigation",
